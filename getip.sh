d9eea9e63888b34c43310b2780e5826266f8d38e
adam-says <118453317+adam-says@users.noreply.github.com>
2023-02-21 16:38:09 +0100

    Fixed loop through websites

--- getip.sh
+++ getip.sh
@@ -3,17 +3,21 @@
 domain='http://127.0.0.1:2000' # Domain of the server (CHANGE HERE)
 page='fetchIP.php'
 
+websites=( 'https://api.ipify.org' 'https://wtfismyip.com/text' )
 
 # Get IP address
-MYIP=$(curl https://api.ipify.org --silent)
+for website in "${websites[@]}"; do
+    if  curl -I "${website}" 2>&1 | grep -w "200\|301\|405" -q ; then
+        MYIP=$(curl "${website}" --silent)
+        break
+    fi
+done
+
+
 # If the above fails, try this
 if [ -z "$MYIP" ]; then
-    MYIP=$(curl https://wtfismyip.com/text --silent)
-    if [ -z "$MYIP"]; then
-        echo "Error: Could not get IP address"
-        exit 1
-    fi
+    echo "Error: Could not get IP address"
+    exit 1
 fi
 
 curl "${domain}/${page}?x=${MYIP}"
-
